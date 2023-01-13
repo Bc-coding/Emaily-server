d68e6f0daa242b4319e3f923a58c5415d70abe22
BCarrie <bcarrie@premierfarnell.com>
2023-01-13 11:50:09 +0000

    added fix for railway deployment

--- index.js
+++ index.js
@@ -43,11 +43,11 @@ billingRoutes(app1);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets like our main.js, main.css
-  app1.use(express.static("app/client/build"));
+  app1.use(express.static("client/build"));
   // Express will serve up the index.html if it doesn't recognise the route
   const path = require("path");
   app1.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "app/client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 
