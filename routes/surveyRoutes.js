04825c5083ca0e1f264f7d498514efbe197d291e
BCarrie <bcarrie@premierfarnell.com>
2023-01-18 15:49:32 +0000

    added mailer service

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const MailerNew = require("../services/MailerNew");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const { mail } = require("sendgrid");
 
 // if we will implement any testing in the future, Mongoose would complaint we imports the model several times if we import directly from models directory
 // the workaround is to import from Mongoose class
@@ -28,6 +32,12 @@ module.exports = (app) => {
     });
 
     // attempt to create and send email
+    const mailer = new Mailer(
+      survey, // data object -- passing survey's body to the template which will be the body of the email
+      surveyTemplate(survey)
+      // template context the recipient will see
+    );
+    mailer.send();
 
     // email sent successfully and then we will save survey
   });
