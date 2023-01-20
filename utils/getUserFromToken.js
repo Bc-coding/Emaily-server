9446ba94a57f7464bc5327ae6355d24d55f6f62a
Belle Carrie <bc.coding@hotmail.com>
2023-01-20 14:25:09 +0000

    clean up

--- utils/getUserFromToken.js
+++ utils/getUserFromToken.js
@@ -1,9 +1,8 @@
 const JWT = require("jsonwebtoken");
 const keys = require("../config/keys");
 
-const getUserFromToken = (token) => {
+const getUserFromToken = token => {
   try {
-    // console.log(token);
     return JWT.verify(token, keys.jwtSinganiture);
   } catch (error) {
     console.log(error);
